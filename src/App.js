3f4eb05c875163040643fb707f33cd9068997688
Alex <alexsalazarny@gmail.com>
2022-02-28 21:12:46 -0500

    Daisy error

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./components/layouts/NavBar";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 import Footer from "./components/layouts/Footer";
 function App() {
   return (
     <Router className="bg-purple-500">
       <div className="flex flex-col justify-between h-screen">
         <NavBar />
-        <main className="container mx-auto px-3 pb-12">Content</main>
-
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            {/* catch all page if we hit a page that doesn't exsist */}
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
         <Footer />
       </div>
     </Router>
---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NoutFound() {
+  return <div>NotFound</div>;
+}
+
+export default NoutFound;
