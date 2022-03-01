0cfe518dcbfc75b8b8f5a5a6ab6c90e5b5e14af0
Alex <alexsalazarny@gmail.com>
2022-02-28 20:44:26 -0500

    Added Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavBar from "./components/layouts/NavBar";
+import Footer from "./components/layouts/Footer";
 function App() {
   return (
     <Router className="bg-purple-500">
       <div className="flex flex-col justify-between h-screen">
         <NavBar />
-        <main>Content</main>
+        <main className="container mx-auto px-3 pb-12">Content</main>
+
+        <Footer />
       </div>
     </Router>
   );
